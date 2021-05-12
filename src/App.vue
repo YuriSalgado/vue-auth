aa7da92cab9b0180b24222a02a17042fa74b34cd
unknown <yuri_salgadinh@hotmail.com>
2021-05-12 19:49:12 -0300

    vue jwt boot

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <a href="">Home</a>
+    <a href="">Login</a>
+    <a href="">Sign Up</a>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
