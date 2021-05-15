0993dbb4344254a2550185396ccc9732c120ebce
unknown <yuri_salgadinh@hotmail.com>
2021-05-14 21:16:27 -0300

    new components Register

---
+++ src/components/Register.vue
@@ -0,0 +1,15 @@
+<template>
+    <form>
+        <h3>Sign Up</h3>
+        <div class="form-group">
+            <label>First Name</label>
+            <input type="text" class="form-control" placeholder="First Name"/>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: 'Register'
+    }
+</script>
