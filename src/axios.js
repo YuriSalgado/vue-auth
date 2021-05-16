513b9dfaaae25b5bd429f88c858276f846e80e0d
unknown <yuri_salgadinh@hotmail.com>
2021-05-16 17:48:11 -0300

    Hello component + api mock

--- src/axios.js
+++ src/axios.js
@@ -1,3 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8000/';
+//axios.defaults.baseURL = 'http://localhost:8000/';
+axios.defaults.baseURL = 'https://60a17790d2855b00173b22c0.mockapi.io/auth/v1/';
+axios.defaults.headers.common['Authorization'] = 'Bearer '+ localStorage.getItem('token');
